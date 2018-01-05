f071eb12b0d71a1e4f9220d3bfcb5b2b80832234
zjlian <zjlianzjlianzjlian@outlook.com>
2018-01-05 22:14:20 +0800

    init this git

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"voting-system-api/tool/rand"
+)
+
+var portCode = flag.String("p", "8080", "web服务监听的端口")
+
+func main() {
+	// text := "zjlian"
+	// fmt.Println(rand.MD5(text))
+	// var l int
+	// fmt.Scanf("%d", &l)
+	// for i := 0; i < l; i++ {
+	// 	fmt.Println(rand.GetRS64())
+	// }
+	var portStr = ":" + (*portCode)
+	fmt.Println("Server running on localhost" + portStr)
+
+	http.HandleFunc("/", greet)
+	http.HandleFunc("/randstr", randstrHandler)
+	http.HandleFunc("/ip", ipHandler)
+	http.ListenAndServe(portStr, nil)
+}
+
+func getRequestIP(r *http.Request) string {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return ""
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return ""
+	}
+	return r.RemoteAddr
+}
+
+func logConnInfo(r *http.Request) {
+	fmt.Println("User " + getRequestIP(r) +
+		" connected and visited this URL: \"" + r.URL.Path + "\"")
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	logConnInfo(r)
+	fmt.Fprintln(w, "Golang server is running")
+}
+
+func randstrHandler(w http.ResponseWriter, r *http.Request) {
+	logConnInfo(r)
+	var printStr string
+	for i := 0; i < 10; i++ {
+		printStr += rand.GetRS32() + "\n"
+	}
+	fmt.Fprintf(w, printStr)
+}
+
+func ipHandler(w http.ResponseWriter, r *http.Request) {
+	logConnInfo(r)
+	fmt.Fprintf(w, "你的IP是: %v", getRequestIP(r))
+}
